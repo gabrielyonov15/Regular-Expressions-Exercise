0a14849d34714b75d7db2024f504872a829b2c3e
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-07-24 20:04:07 +0300

    Add files via upload

---
+++ 06.ExtractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string emailPattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-][A-Za-z]+)+))(\b|(?=\s))";
+            MatchCollection emailMatches = Regex.Matches(text, emailPattern);
+            foreach (Match email in emailMatches)
+            {
+                Console.WriteLine(email.Value);
+            }
+        }
+    }
+}
